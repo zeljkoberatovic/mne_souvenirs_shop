f6603a1b6467bcf58d6ba344d11bbb1bf3097f0e
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-01-15 13:41:43 +0100

    first commit in mne_souvenirs_shop

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.base')
+@section('content')
+
+<h1>User Dashboard</h1>
+    
+@endsection
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                // 'resources/sass/app.css',
+                'resources/app.js',  
+            ],
+            refresh: true,
+        }),
+    
+        // Automatsko osvjezavanje stranice nakon izvrsenih izmjena
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if(file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            }
+        }
+    ],
+});
